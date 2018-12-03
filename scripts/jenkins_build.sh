6b1f7efa80ead99f9815c56441fc5537ec36e20f
alexgroom <agroom@redhat.com>
2018-12-03 14:28:39 +0000

    Build scripts

---
+++ scripts/jenkins_build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+oc new-project tasks-build
+oc new-app jboss-eap71-openshift:1.3~https://github.com/wkulhanek/openshift-tasks --name tasks
+oc set triggers dc tasks --manual
+oc expose svc tasks
+
+oc new-project tasks-dev
+oc new-app tasks-build/tasks:dev --name=tasks --allow-missing-images -n tasks-dev
+oc set triggers dc tasks --manual
+oc expose dc/tasks --port 8080 -n tasks-dev
+oc expose svc tasks
+
+oc new-project tasks-test
+oc new-app tasks-build/tasks:test --name=tasks --allow-missing-images -n tasks-test
+oc set triggers dc tasks --manual
+oc expose dc/tasks --port 8080 -n tasks-test
+oc expose svc tasks
+
+oc new-project tasks-prod
+oc new-app tasks-build/tasks:prod --name=tasks --allow-missing-images -n tasks-prod
+oc set triggers dc tasks --manual
+oc expose dc/tasks --port 8080 -n tasks-prod
+oc expose svc tasks
+
+oc new-project cicd-dev 
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+oc policy add-role-to-user edit system:serviceaccount:cicd-dev:jenkins -n tasks-build
+oc policy add-role-to-user edit system:serviceaccount:cicd-dev:jenkins -n tasks-dev
+oc policy add-role-to-user edit system:serviceaccount:cicd-dev:jenkins -n tasks-test
+oc policy add-role-to-user edit system:serviceaccount:cicd-dev:jenkins -n tasks-prod
+oc policy add-role-to-group system:image-puller system:serviceaccounts:tasks-dev -n tasks-build
+oc policy add-role-to-group system:image-puller system:serviceaccounts:tasks-test -n tasks-build
+oc policy add-role-to-group system:image-puller system:serviceaccounts:tasks-prod -n tasks-build
+oc create -f jenkins_template.yml
+echo "Start the build"
+oc start-build tasks-pipeline
+
